a7722770c02f4ee83a3db7e24b50854f8ca624d8
Sakib Mahmud <sakibmahmudacdc@gmail.com>
2022-06-01 16:59:29 +0600

    gg

--- avg.js
+++
@@ -1,9 +0,0 @@
-function calculateBmi() {
-    var runs = document.bmif.runs.value
-    var outs = document.bmif.outs.value
-    if(runs > 0 && outs > 0){	
-    var finalAvg = runs/outs
-    
-    document.bmif.bmi.value = finalAvg.toFixed(2);
-    }
-} 
--- strike.js
+++
@@ -1,8 +0,0 @@
-function calculateFI() {
-    var feet = document.hei.feet.value;
-    var inch = document.hei.inch.value;
-
-    var finalFI = (feet*100)/inch;
-    
-    document.hei.hhh.value = finalFI.toFixed(3);
-}
